c0a1d218177b38359297e53b465316ffdc8c69a7
Steve Waldman <swaldman@mchange.com>
2023-03-23 16:59:08 -0400

    Works at least roughly...

--- generator/src/com/mchange/unproject/seed/core.scala
+++ generator/src/com/mchange/unproject/seed/core.scala
@@ -32,20 +32,28 @@ val SeedUnstaticSrcDir    = SeedsStaticSrcDir / "unstatic"
 val SeedUntemplateBuildScFile = UntemplateSeedOutDir / "src" / "main" / "g8" / "build.sc"
 val SeedUnstaticBuildScFile   = UnstaticSeedOutDir / "src" / "main" / "g8" / "build.sc"
 
+val UntemplateMergeDir = os.pwd / os.up / UntemplateSeedDirName
+val UnstaticMergeDir   = os.pwd / os.up / UnstaticSeedDirName
+
 @main
-def rebuildSeeds =
+def rebuildSeeds( merge : Boolean = false ) =
   os.remove.all( SeedsOutDir ) // danger warning this is rm -rf 
 
   os.makeDir.all( UntemplateSeedOutDir )
-  os.copy( from = SeedCommonSrcDir, to = UntemplateSeedOutDir, mergeFolders = true )
-  os.copy( from = SeedUntemplateSrcDir, to = UntemplateSeedOutDir, mergeFolders = true )
+  os.copy( from = SeedCommonSrcDir, to = UntemplateSeedOutDir, copyAttributes = true, mergeFolders = true )
+  os.copy( from = SeedUntemplateSrcDir, to = UntemplateSeedOutDir, copyAttributes = true, mergeFolders = true )
   os.write( target = SeedUntemplateBuildScFile, data = UntemplateBuildScText )
 
   os.makeDir.all( UntemplateSeedOutDir )
-  os.copy( from = SeedCommonSrcDir, to = UnstaticSeedOutDir, mergeFolders = true )
-  os.copy( from = SeedUnstaticSrcDir, to = UnstaticSeedOutDir, mergeFolders = true )
+  os.copy( from = SeedCommonSrcDir, to = UnstaticSeedOutDir, copyAttributes = true, mergeFolders = true )
+  os.copy( from = SeedUnstaticSrcDir, to = UnstaticSeedOutDir, copyAttributes = true, mergeFolders = true )
   os.write( target = SeedUnstaticBuildScFile, data = UnstaticBuildScText )
 
+  if merge then
+    os.makeDir.all( UntemplateMergeDir )
+    os.copy( from = UntemplateSeedOutDir, to = UntemplateMergeDir, replaceExisting = true, copyAttributes = true, mergeFolders = true )
+    os.makeDir.all( UnstaticMergeDir )
+    os.copy( from = UnstaticSeedOutDir, to = UnstaticMergeDir, replaceExisting = true, copyAttributes = true, mergeFolders = true )
 
 
 
